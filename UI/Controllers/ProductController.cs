c9e566e8e874478a29efa478c9872e82eb820b5a
ezgiciftcil <83506824+ezgiciftcil@users.noreply.github.com>
2022-05-26 16:08:10 +0300

    .

---
+++ UI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Services.Interfaces;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+using UI.Models;
+
+namespace UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        ICategoryService categoryService;
+        public ProductController(ICategoryService _categoryService)
+        {
+            categoryService = _categoryService;
+        }
+        public IActionResult Index(int categoryId, string categoryName)
+        {
+            var productsOfCategory = new ProductsOfCategoryModel();
+            productsOfCategory.Categories = categoryService.GetAllCategories().Data;
+            productsOfCategory.CategoryId = categoryId;
+            productsOfCategory.CategoryName = categoryName;
+            return View(productsOfCategory);
+        }
+    }
+}
---
+++ UI/Models/ProductsOfCategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Models
+{
+    public class ProductsOfCategoryModel
+    {
+        public List<Category> Categories { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
