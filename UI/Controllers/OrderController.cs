fce7271774c0631f61fd94ed8602dd0180addc9c
ezgiciftcil <83506824+ezgiciftcil@users.noreply.github.com>
2022-05-28 13:29:58 +0300

    .

--- UI/Controllers/OrderController.cs
+++ UI/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Services.Interfaces;
+using EntityLayer;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,9 +13,11 @@ namespace UI.Controllers
     public class OrderController : Controller
     {
         private readonly IAddressService addressService;
-        public OrderController(IAddressService _addressService)
+        private readonly ICityService cityService;
+        public OrderController(IAddressService _addressService, ICityService _cityService)
         {
             addressService = _addressService;
+            cityService = _cityService;
         }
         public IActionResult Checkout(string json)
         {
@@ -24,11 +27,27 @@ namespace UI.Controllers
 
             var model = new OrderModel();
             model.Cart= Newtonsoft.Json.JsonConvert.DeserializeObject<CartModel>(json);
+            model.Cart.CartModelJson = json;
 
             var defaultAddress = addressService.GetUserDefaultAddress(userId);
             model.IsDefaultAddressExists = defaultAddress.Success;
+            if (model.IsDefaultAddressExists)
+                model.Address = defaultAddress.Data;
+
+            var cityList = new CityList(cityService);
+            var cities = cityList.GetCities();
+            ViewBag.cities = cities;
+
 
             return View(model);
         }
+
+        [HttpPost]
+        public IActionResult Checkout(OrderModel orderModel)
+        {
+            orderModel.Cart = Newtonsoft.Json.JsonConvert.DeserializeObject<CartModel>(orderModel.Cart.CartModelJson);
+            return View();
+        }
+
     }
 }
